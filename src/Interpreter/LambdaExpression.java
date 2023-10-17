d36c7e7caa69e7f8355ed4de645047afe9e9ba9b
Joshua Tint <tintjosh@gmail.com>
2023-10-16 18:45:48 -0700

    correct lambda expression AST node body

--- src/Interpreter/LambdaExpression.java
+++ src/Interpreter/LambdaExpression.java
@@ -1,5 +1,21 @@
 package Interpreter;
 
+import Elements.Type;
+
 public class LambdaExpression extends Expression {
 
+    @Override
+    public ExpressionResult evaluate(State situatedState) {
+        return null;
+    }
+
+    @Override
+    public ValidationContext validate(ValidationContext context) {
+        return null;
+    }
+
+    @Override
+    public Type getType(ValidationContext context) {
+        return null;
+    }
 }
